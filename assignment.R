6c0a0bcea83febec1d737f9b4dc2260620bfcbbc
locusclassicus <alieva.mgl@gmail.com>
2025-10-29 13:29:53 +0300

    submit hw xml

--- assignment.R
+++ assignment.R
@@ -15,41 +15,62 @@ library(dplyr)
 library(purrr)
 
 unzip("letters.zip")
+
 my_xmls <- list.files("letters/", full.names = TRUE)
 
-# Сначала напишите код для первого письма в датасете, чтобы потренироваться. 
-
+# пишем код для одного письма
 test_xml <- my_xmls[1]
-doc <- # ваш код здесь
-ns <- # ваш код здесь
+doc <- read_xml(test_xml)
+ns <- xml_ns(doc)
 
-  
 # дата письма
-date <- # ваш код здесь
+date <- xml_find_first(doc, "//d1:correspAction[@type='sending']//d1:date", ns) |> 
+  xml_attr("when") 
+
 
 
 # адресат письма
-corresp <- # ваш код здесь
+corresp <- xml_find_first(doc, "//d1:correspAction[@type='receiving']//d1:persName", ns) |> 
+  xml_text() |> 
+  trimws()
+
+
 
 # том 
-vol <- # ваш код здесь
+
+vol <- xml_find_first(doc, "//d1:biblScope[@unit='vol']", ns) |> 
+  xml_text() |> 
+  trimws()
 
 
-## Когда все получится, оберните свое решение в функцию read_letter().
-
+### ---------------------------###
+## теперь оборачиваем в функцию
 read_letter <- function(xml_path) {
+  doc <- read_xml(xml_path)
+  ns <- xml_ns(doc)
   
-  # ваш код здесь 
-
+  # дата письма
+  date <- xml_find_first(doc, "//d1:correspAction[@type='sending']//d1:date", ns) |> 
+    xml_attr("when") 
+  
+  # адресат письма
+  corresp <- xml_find_first(doc, "//d1:correspAction[@type='receiving']//d1:persName", ns) |> 
+    xml_text() |> 
+    trimws()
+  
+  vol <- xml_find_first(doc, "//d1:biblScope[@unit='vol']", ns) |> 
+    xml_text() |> 
+    trimws()
+  
   # записываем в тиббл
   res <- tibble(
-    # ваш код здесь
-   )
-
+    date = date,
+    corresp = corresp,
+    vol = vol
+  )
+  
   return(res)
 }
 
 
-# Прочтите все письма в один тиббл при помощи map_dfr(). 
-letters_tbl <- # ваш код здесь
-
+letters_tbl <- map_dfr(my_xmls, read_letter)
